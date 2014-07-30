0e56ef5aa8fb880b81c84e67563fdcc6f52d872a
Emil Hemdal <emil.hemdal.em@gmail.com>
2014-07-30 13:25:49 +0200

    improved parsing by making it parse using one object rather than two which was confusing

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 	// We use the steam functionality of sax since the MEDLINE files tend to be quite large and to load a 100MB large XML file 
 	// into memory is a bad idea when Node.js can process it on the go saving memory.
 	var parser = sax.createStream(true, { // true means XML parsing
-		trim: true, // Trims text and comment nodes
+		//trim: true, // Trims text and comment nodes
 		normalize: true // Turnes any whitespace into a single space
 	});
 
@@ -41,15 +41,16 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 		elocationIndex = -1,
 		lastDateTag = "",
 		issnType = "",
-
-		nodeParser = {
-			"MedlineCitationSet": function(nodeData) { return; },
-			"DeleteCitation": function(nodeData) { console.log("\n\n\nDELETE FOUND!\n\n\n"); },
-			"MedlineCitation": function(nodeData) {
+		authorListRun = false,
+		nodeData = null,
+		textParser = {
+			"DeleteCitation": function(text) { console.log("\n\n\nDELETE FOUND!\n\n\n"); },
+			"MedlineCitation": function(text) {
 				index++;
 				abstractIndex = -1;
 				elocationIndex = -1;
 				authorIndex = -1;
+				authorListRun = false;
 				/*json[index] = {
 					"_id": 			null,
 					"owner": 		nodeData.attributes.Owner,
@@ -107,72 +108,12 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 				};
 			},
-			"PMID": function(nodeData) { return; },
-			"DateCreated": function(nodeData) { lastDateTag = "created"; },
-			"DateCompleted": function(nodeData) { lastDateTag = "completed"; json[index].dates.completed = {} },
-			"DateRevised": function(nodeData) { lastDateTag = "revised"; json[index].dates.revised = {} },
-			"Year": function(nodeData) { return; },
-			"Month": function(nodeData) { return; },
-			"Day": function(nodeData) { return; },
-			"Volume": function(nodeData) { return; },
-			"Issue": function(nodeData) { return; },
-			"PubDate": function(nodeData) { lastDateTag = "pubdate"; },
-			"Article": function(nodeData) { json[index].pubmodel = nodeData.attributes.PubModel; },
-			"Journal": function(nodeData) { return; },
-			"JournalIssue": function(nodeData) { json[index].journal.citedMedium = nodeData.attributes.CitedMedium },
-			"Title": function(nodeData) { return; },
-			"ISOAbbreviation": function(nodeData) { return; },
-			"ArticleDate": function(nodeData) { lastDateTag = "articledate"; json[index].articledate = {} },
-			"ISSN": function(nodeData) { issnType = nodeData.attributes.IssnType; },
-			"ELocationID": function(nodeData) {
-				elocationIndex++;
-				if(typeof json[index].pagnation.elocation !== "object") {
-					json[index].pagnation.elocation = new Array( { "type": nodeData.attributes.EIdType } );
-				} else {
-					json[index].pagnation.elocation.push( { "type": nodeData.attributes.EIdType } );
-				}
-			},
-			"Abstract": function(nodeData) { json[index].abstract = {}; },
-			"AbstractText": function(nodeData) {
-				if(whereAmI[whereIndex-2] === "Article") {
-					abstractIndex++;
-					if(typeof json[index].abstract.abstracts !== "object") {
-						json[index].abstract.abstracts = new Array( { "text": "", "label": nodeData.attributes.Label, "nlmCategory": nodeData.attributes.NlmCategory } );
-					} else {
-						json[index].abstract.abstracts.push( { "text": "", "label": nodeData.attributes.Label, "nlmCategory": nodeData.attributes.NlmCategory } );
-					}
-				} else {
-					console.log("TODO!!!");
-				}
-			},
-			"AuthorList": function(nodeData) {
-				json[index].authors = new Array();
-			},
-			"Author": function(nodeData) {
-				authorIndex++;
-				json[index].authors.push({});
-			},
-			"CopyrightInformation": function(nodeData) {
-				// TODO
-			},
-			"KeywordList": function(nodeData) {
-				if(typeof json[index].keywordList !== "object") {
-					json[index].keywordList = new Array( { "owner": nodeData.attributes.Owner, "list": new Array() } );
-				} else {
-					json[index].keywordList.push( { "owner": nodeData.attributes.Owner, "list": new Array() } );
-				}
-			},
-			"Keyword": function(nodeData) {
-				json[index].keywordList[json[index].keywordList.length-1].list.push( { "major": ( nodeData.attributes.MajorTopicYN === "Y" ? true : false ) } );
-			},
-			"_default": function(nodeData) {
-				//if(Object.keys(nodeData.attributes).length !== 0) {
-					console.log(JSON.stringify(nodeData));
-				//}
-			}
-		},
-		textParser = {
 			"PMID": function(text) { json[index]._id = text; },
+			"DateCreated": function(text) { lastDateTag = "created"; },
+			"DateCompleted": function(text) { lastDateTag = "completed"; json[index].dates.completed = {} },
+			"DateRevised": function(text) { lastDateTag = "revised"; json[index].dates.revised = {} },
+			"PubDate": function(text) { lastDateTag = "pubdate"; },
+			"ArticleDate": function(text) { lastDateTag = "articledate"; json[index].articledate = {} },
 			"Year": function(text) {
 				if(["created", "completed", "revised"].indexOf(lastDateTag) !== -1) {
 					json[index].dates[lastDateTag].year = text;
@@ -200,6 +141,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 					json[index].articledate.day = text;
 				}
 			},
+			"Article": function(text) { json[index].pubmodel = nodeData.attributes.PubModel; },
 			"Volume": function(text) { json[index].journal.volume = text; },
 			"Issue": function(text) { json[index].journal.issue = text; },
 			"ArticleTitle": function(text) { json[index].article = text; },
@@ -210,13 +152,50 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 					json[index].pagnation.lp = text[1];
 				}
 			},
-			"ELocationID": function(text) { json[index].pagnation.elocation[elocationIndex].value = text; },
-			"AbstractText": function(text) { json[index].abstract.abstracts[abstractIndex].text = text; },
+			"JournalIssue": function(text) { json[index].journal.citedMedium = nodeData.attributes.CitedMedium },
+			"ELocationID": function(text) {
+				elocationIndex++;
+				if(typeof json[index].pagnation.elocation !== "object") {
+					json[index].pagnation.elocation = new Array( { "type": nodeData.attributes.EIdType } );
+				} else {
+					json[index].pagnation.elocation.push( { "type": nodeData.attributes.EIdType } );
+				}
+				json[index].pagnation.elocation[elocationIndex].value = text;
+			},
+			"AbstractText": function(text) {
+				if(whereAmI[whereIndex-2] === "Article") {
+					abstractIndex++;
+					if(json[index].abstract === undefined) {
+						json[index].abstract = {};
+					}
+					if(typeof json[index].abstract.abstracts !== "object") {
+						json[index].abstract.abstracts = new Array( { "text": "", "label": nodeData.attributes.Label, "nlmCategory": nodeData.attributes.NlmCategory } );
+					} else {
+						json[index].abstract.abstracts.push( { "text": "", "label": nodeData.attributes.Label, "nlmCategory": nodeData.attributes.NlmCategory } );
+					}
+				} else {
+					console.log("TODO!!!");
+				}
+				json[index].abstract.abstracts[abstractIndex].text = text;
+			},
 			"CopyrightInformation": function(text) { json[index].abstract.copyright = text; },
 			"Language": function(text) { json[index].language = text; },
-			"ISSN": function(text) { json[index].journal[(issnType === "Electronic" ? "eissn" : "issn")] = text; },
+			"ISSN": function(text) {
+				issnType = nodeData.attributes.IssnType;
+				json[index].journal[(issnType === "Electronic" ? "eissn" : "issn")] = text;
+			},
 			"Title": function(text) { json[index].journal.title = text; },
 			"ISOAbbreviation": function(text) { json[index].journal.isoAbbreviation = text; },
+			"AuthorList": function(text) {
+				if(!authorListRun) {
+					authorListRun = true;
+					json[index].authors = new Array();
+				}
+			},
+			"Author": function(text) {
+				authorIndex++;
+				json[index].authors.push({});
+			},
 			"LastName": function(text) {
 				if(whereAmI[whereIndex-1] === "Author") {
 					json[index].authors[authorIndex].lastName = text;
@@ -278,23 +257,39 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 			"ISSNLinking": function(text) {
 				json[index].journal.lissn = text;
 			},
+			"KeywordList": function(text) {
+				if(typeof json[index].keywordList !== "object") {
+					json[index].keywordList = new Array( { "owner": nodeData.attributes.Owner, "list": new Array() } );
+				} else {
+					json[index].keywordList.push( { "owner": nodeData.attributes.Owner, "list": new Array() } );
+				}
+			},
 			"Keyword": function(text) {
+				json[index].keywordList[json[index].keywordList.length-1].list.push( { "major": ( nodeData.attributes.MajorTopicYN === "Y" ? true : false ) } );
 				json[index].keywordList[json[index].keywordList.length-1].list[json[index].keywordList[json[index].keywordList.length-1].list.length-1].text = text;
 			},
-			"_default": function(text) { console.log(text); }
+			"_default": function(text) {
+				console.log(JSON.stringify(nodeData));
+				console.log(text);
+			}
 		};
 
-	parser.on("opentag", function(nodeData) { // Runs when an XML tag is opened. The nodeData variable contains the name of the tag and it's attributes.
-		whereAmI.push(nodeData.name);
+	parser.on("opentag", function(data) { // Runs when an XML tag is opened. The data variable contains the name of the tag and it's attributes.
+		whereAmI.push(data.name);
 		whereIndex++;
-		nodeParser[nodeData.name] ? nodeParser[nodeData.name](nodeData) : nodeParser._default(nodeData);
+		nodeData = data;
 	});
 	parser.on("closetag", function(nodeName) { // Runs when an XML tag is closed.
 		whereAmI.pop();
 		whereIndex--;
 	});
 	parser.on("text", function(text) { // Runs when there is text in an XML tag, not an attribute.
-		textParser[whereAmI[whereIndex]] ? textParser[whereAmI[whereIndex]](text) : textParser._default(text);
+		// The following two lines fixes the bug in sax when text is run twice, once when the tag opens and once when the tag closes.
+		if(whereAmI.length > 0) {
+			if(whereAmI[whereAmI.length-1] === nodeData.name) {
+				textParser[whereAmI[whereIndex]] ? textParser[whereAmI[whereIndex]](text) : textParser._default(text);
+			}
+		}
 	});
 	parser.on("end", function(text) { // Runs when all the XML processing is done.
 		console.log("\nEND JSON:\n");
