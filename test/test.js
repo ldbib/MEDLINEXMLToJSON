5dbb1e07ba3c145ab61da1108486e84a4772b761
Emil Hemdal <emil.hemdal.em@gmail.com>
2014-08-06 17:28:47 +0200

    add copyright to test.js

--- test/test.js
+++ test/test.js
@@ -1,3 +1,25 @@
+/*
+
+Copyright 2014 Emil Hemdal <emil(at)hemdal(dot)se>
+Copyright 2014 Landstinget Dalarna Bibliotek och informationscentral <webmaster(dot)lasarettsbiblioteken(at)ltdalarna(dot)se>
+
+This file is part of MEDLINEXMLToJSON.
+
+Foobar is free software: you can redistribute it and/or modify
+it under the terms of the GNU Affero General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Foobar is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Affero General Public License for more details.
+
+You should have received a copy of the GNU Affero General Public License
+along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+
+*/
+
 m2json = require("../lib/app.js");
 
 console.log('Current directory: ' + process.cwd());
